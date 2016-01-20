7455d98610b7a31102702370a18800b8d3048bb6
Matias Insaurralde <matias@insaurral.de>
2016-01-20 00:02:29 -0300

    simple route

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require( 'express' ),
-    app = express(),
-    bodyParser = require( 'body-parser' )
-
-var port = process.env.PORT || 3000
+    bodyParser = require( 'body-parser' ),
+    port = process.env.PORT || 3000,
+    app = express()
 
 app.use( bodyParser.json() )
+
+var router = express.Router()
+
+router.get( '/', function( req, res ) {
+  res.json({
+    message: 'hello!'
+  })
+})
+
+app.use( '/api', router )
 
 app.listen( port )
