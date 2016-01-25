b862a367fc393a649392dc2e57a608496223c5e4
Matias Insaurralde <matias@insaurral.de>
2016-01-25 12:01:44 -0300

    get/post & changing default port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require( 'express' ),
     bodyParser = require( 'body-parser' ),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 3001,
     app = express()
 
 app.use( bodyParser.json() )
@@ -8,11 +8,19 @@ app.use( bodyParser.json() )
 var router = express.Router()
 
 router.get( '/', function( req, res ) {
+  console.log( 'get' )
   res.json({
     message: 'hello!'
   })
 })
 
-app.use( '/api', router )
+router.post( '/', function( req, res ) {
+  console.log( 'post' )
+  res.json({
+    message: 'hello!'
+  })
+})
+
+app.use( '/', router )
 
 app.listen( port )
